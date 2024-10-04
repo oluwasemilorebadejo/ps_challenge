93a0686cf268697ba6dc84c725d5433dc292316c
oluwasemilorebadejo <badejo.semilore.7@gmail.com>
2024-10-04 11:26:28 +0100

    add user and room entity

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,5 +1,7 @@
 import { DataSource } from "typeorm";
 import config from "config";
+import User from "./entity/User";
+import Room from "./entity/Room";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
@@ -10,7 +12,7 @@ export const AppDataSource = new DataSource({
   database: config.get<string>("database"),
   synchronize: true,
   logging: false,
-  entities: [],
+  entities: [User, Room],
   migrations: [],
   subscribers: [],
 });
